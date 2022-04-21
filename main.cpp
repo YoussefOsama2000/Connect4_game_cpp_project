7fa9d4b4ab3e9c6bd2e42fc81b805c62d6f82d6b
YoussefOsama200 <youssef.ali001@eng-st.cu.edu.eg>
2022-04-22 00:51:07 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+using namespace std;
+class GameEngine
+{
+    char board[6][7];
+    // declareing the board in the heap to avoid stack overflow
+public:
+    void printBoard()
+    {
+        cout<<endl<<endl<<"     1   2   3   4   5   6   7"<<endl;
+        for (int i = 0; i < 6; i++)
+        {
+            cout << " |   ";
+            for (int j = 0; j < 7; j++)
+            {
+                cout <<board[i][j] << "   ";
+            }
+            cout << "| " << endl;
+        }
+        cout<<"   ____________________________"<<endl;
+    }
+void enterPlayer1turn(int column){
+
+    }
+
+//    GameEngine(){
+//        for(int i=0;i<7;i++) {
+//            for (int j = 0; j < 6; i++) { board[i][j] = '-'; }
+//        }
+//    };
+};
+
+
+int main()
+{
+    GameEngine gameEngine = GameEngine();
+    gameEngine.printBoard();
+}
